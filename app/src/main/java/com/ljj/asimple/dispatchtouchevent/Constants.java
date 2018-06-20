ce4d884378b85db3799cf4e2b5f8eab05049f348
lijunjie <lijunjie@zenmen.com>
2018-06-20 17:55:41 +0800

    add touchevent

--- app/src/main/java/com/ljj/asimple/dispatchtouchevent/Constants.java
+++ app/src/main/java/com/ljj/asimple/dispatchtouchevent/Constants.java
@@ -1,6 +1,6 @@
 package com.ljj.asimple.dispatchtouchevent;
 
 public class Constants {
-    public final static String TAG = "MY_LOG";
-    public final static String MY_GESTURE_TAG = "GESTURE_TAG";
+    public final static String TAG = "touch_event";
+    public final static String MY_GESTURE_TAG = "gesture_event";
 }
